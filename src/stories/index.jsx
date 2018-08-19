68f293c93efbb161b1da4ecf65aca974dcc79b72
Jonathan Abrahams <jonathanabrahams@gmail.com>
2018-08-19 20:36:19 +0200

    storybook

--- src/stories/index.jsx
+++ src/stories/index.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
+  .addDecorator(withKnobs)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
@@ -16,4 +18,9 @@ storiesOf('Button', module)
         😀 😎 👍 💯
       </span>
     </Button>
-  ));
+  ))
+  .add('with disabled', () => (
+    <Button disabled={boolean('Disabled', false)} onClick={action('clicked')}>
+      {text('Lable', 'Hello storybook')}
+    </Button>)
+  );
